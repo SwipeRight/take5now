31cf0a6d8b9cc0c1a45f088fed0e6e358208bf7c
SwipeRight <SwipeRight@outlook.ie>
2016-01-21 19:37:50 +0000

    Extending Database

---
+++ take5/Database/Matches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class Matches
+    {
+        private int index { get; set; }
+        private int userID1 { get; set; }
+        private int userID2 { get; set; }
+        //1 - Pool
+        //2 - Coffee
+        //3 - Tenis
+        //4 - Walk
+        private int activity { get; set; }
+        private long timestamp { get; set; }
+
+        public void newMatch(int index, int UserID1, int UserID2, int Activity, long timeStamp)
+        {
+            this.index = index;
+            this.userID1 = UserID1;
+            this.userID2 = UserID2;
+            this.activity = Activity;
+            this.timestamp = timeStamp;
+        }
+
+    }
+}
--- take5/take5/Classes/DatabaseConnection.cs
+++ take5/take5/Classes/DatabaseConnection.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace take5.Classes
 {
@@ -11,6 +13,7 @@ namespace take5.Classes
         public DatabaseConnection()
         {
             //connection string  Server=tcp:swiperight.database.windows.net,1433;Database=SwipeRightDB;User ID=SwipeRight@swiperight;Password={your_password_here};Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+            MyDatabase test = new MyDatabase();
         }
     }
 }
